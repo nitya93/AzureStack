159ec1bd95dd189ad2d3352f0ed0ad9e6c3903ef
nitya93 <nitya93@gmail.com>
2017-04-20 14:51:57 -0700

    Update configure-app.ps1

--- configure-app.ps1
+++ configure-app.ps1
@@ -2,5 +2,7 @@ netsh advfirewall set all state off
 
 New-Item -Path "c:\" -Name "Netperf" -ItemType "directory"
 New-Item -Path "c:\Netperf" -Name "Test" -ItemType "directory"
+Invoke-WebRequest https://github.com/nitya93/AzureStack/edit/master/Sample.txt -OutFile c:\Netperf\Sample.txt
+
 
 
